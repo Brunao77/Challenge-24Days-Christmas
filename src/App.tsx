b918cd51d726501f568b8818f5d2c0b27fc92bbd
Brunao77 <88481339+Brunao77@users.noreply.github.com>
2021-12-01 15:04:26 -0300

    Day 1 changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [gifts, setGifts] = useState(["Medias", "Caramelos", "Vitel Tone"]);
+
   return (
     <div className="App">
       <h1>Regalos:</h1>
-      ...
+      {gifts.map((gift) => (
+        <p>{gift}</p>
+      ))}
     </div>
   );
 }
