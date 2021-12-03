535b9fb902003477b6fb0d1623403ea9f6b6de60
Brunao77 <88481339+Brunao77@users.noreply.github.com>
2021-12-02 22:34:08 -0300

    Day 3 Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ import "./styles.css";
 export default function App() {
   const [gifts, setGifts] = useState(["Medias", "Caramelos", "Vitel Tone"]);
 
+  const handleSubmit = (event) => {
+    setGifts(event.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Regalos:</h1>
+      <input
+        type="submit"
+        placeholder="Escriba su regalo aquí..."
+        onSubmit={handleSubmit}
+      ></input>
       {gifts.map((gift, index) => (
         <p key={index} className="gift-text">
           {gift}
