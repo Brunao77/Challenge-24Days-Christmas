2b68841b761c1c8a5bc0d099db859346a3dd27f4
Brunao77 <88481339+Brunao77@users.noreply.github.com>
2021-12-04 13:03:20 -0300

    Day 3 Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
+import { BsFillPencilFill } from "react-icons/bs";
 
 export default function App() {
+  const giftRef = useRef();
+
   const [gifts, setGifts] = useState(["Medias", "Caramelos", "Vitel Tone"]);
 
-  const handleSubmit = (event) => {
-    setGifts(event.target.value);
+  const handleAdd = () => {
+    const gift = giftRef.current.value;
+    if (gift === "") return;
+
+    setGifts([gift, ...gifts]);
+
+    giftRef.current.value = null;
   };
 
   return (
     <div className="App">
       <h1>Regalos:</h1>
-      <input
-        type="submit"
-        placeholder="Escriba su regalo aquí..."
-        onSubmit={handleSubmit}
-      ></input>
+      <div className="container-form">
+        <input
+          ref={giftRef}
+          type="text"
+          placeholder="Escriba su regalo aquí..."
+        />
+        <button type="submit" className="add-btn" onClick={handleAdd}>
+          <BsFillPencilFill className="icon-pen" />
+        </button>
+      </div>
       {gifts.map((gift, index) => (
         <p key={index} className="gift-text">
           {gift}
